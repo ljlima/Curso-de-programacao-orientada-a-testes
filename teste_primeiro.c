4eab7b50da723bf6b70f92ac954c0f0a678b6b53
ljlima <limajeanlucas@gmail.com>
2022-11-05 20:41:16 -0400

    teste

---
+++ teste_primeiro.c
@@ -0,0 +1,24 @@
+#include "src/unity.h"
+#include "primeiraFuncao.h"
+
+void setUp(){};
+
+void tearDown(){};
+
+void test_funcao(){
+   TEST_ASSER_EQUAL();
+}
+
+
+int main(void){
+   UNITY_BEGIN();
+   RUN_TEST(test_funcao);
+   return UNITY_END();
+
+}
+
+//Novos testes
+//mais testes
+
+
+///nnnnnnnnnnnnnnnnnnnnnnnnn
